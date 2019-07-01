f0456b2cad493e4ed9a8b38d91628ad0af595778
andrew-sharpl <jasharpluevano@penguin>
2019-07-01 15:20:49 -0400

    installed express

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+// Set these as your evnironment variables (run source env.sh)
 const accountSid = process.env.accountSid;
 const authToken = process.env.authToken;
+const sendTo = process.env.sendTo;
+const sendFrom = process.env.sendTo;
+if (!accountSid) throw new Error("No account sid provided");
+if (!authToken) throw new Error("No auth token provided");
 
-console.log(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
+
+client.messages
+  .create({
+    body: "Howdy!!",
+    from: process.env.sendFrom,
+    to: process.env.sendTo
+  })
+  .then(message => console.log(message.sid));
