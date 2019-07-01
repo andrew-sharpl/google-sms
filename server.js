419103e03bfbc6cce60c07aaabe498b8322b3513
andrew-sharpl <jasharpluevano@penguin>
2019-07-01 18:04:32 -0400

    Removed ngrok

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const http = require('http');
-const express = require('express');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const http = require("http");
+const express = require("express");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
 
 const app = express();
 
-app.post('/sms', (req, res) => {
-    const twiml = new MessagingResponse();
+app.post("/sms", (req, res) => {
+  const twiml = new MessagingResponse();
 
-    twiml.message('The robots are coming!');
+  twiml.message("The robots are coming!");
 
-    res.writeHead(200, {'Content-Type': 'text/xml'})
-    res.end(twiml.toString());
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(twiml.toString());
 });
 
 app.listen(1337, () => {
-    console.log('Express server listening on port 1337');
-})
+  console.log("Express server listening on port 1337");
+});
