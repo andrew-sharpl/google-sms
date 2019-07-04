ba28fbe490a78671702dbdde77c8f3d8de4090e0
andrew-sharpl <jasharpluevano@penguin>
2019-07-04 14:26:51 -0400

    SMS receive working!

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 // Set these as your evnironment variables (run source env.sh)
 const accountSid = process.env.accountSid;
 const authToken = process.env.authToken;
-const sendTo = process.env.sendTo;
-const sendFrom = process.env.sendTo;
 if (!accountSid) throw new Error("No account sid provided");
 if (!authToken) throw new Error("No auth token provided");
 
-const client = require("twilio")(accountSid, authToken);
+const express = require("express");
+const Twilio = require("twilio");
+const MessagingResponse = Twilio.twiml.MessagingResponse;
+const app = express();
+const client = Twilio(accountSid, authToken);
 
-client.messages
-  .create({
-    body: "Howdy!!",
-    from: process.env.sendFrom,
-    to: process.env.sendTo
-  })
-  .then(message => {console.log(message);});
+app.post("/sms", (req, res) => {
+  // const twiml = new MessagingResponse();
+  client.messages
+  .list({ limit: 1, to: process.env.sendFrom })
+  .then(messages => messages.forEach(m => console.log(m.body)));
+  // twiml.message("Hello there");
+  // res.writeHead(200, { "Content-Type": "text/xml" });
+  // res.end(twiml.toString());
+});
 
+app.listen(1337, () => {
+  console.log("Express server listening on port 1337");
+});
+
+async function getSids() {
+  var sids = await client.messages.list();
+  console.log(sids);
+}
